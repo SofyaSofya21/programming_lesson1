5de1ccf7a6fa84f353a422b8ed33002bd9d54a76
Sofya <sofyaguseva1@gmail.comgit>
2022-11-10 03:29:22 +0100

    Example 005 created

---
+++ Examples/Example005_Conditions/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "соня")
+{
+    Console.WriteLine("Ура! Привет, Сонечка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
