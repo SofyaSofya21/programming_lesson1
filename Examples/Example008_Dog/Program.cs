d744d834deaf177ac8d03102a5961504f8627453
Sofya <sofyaguseva1@gmail.comgit>
2022-11-11 00:55:59 +0100

    Example 008 created

---
+++ Examples/Example008_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 10000;
+double firstFriendSpeed = 1;
+double secondFriendSpeed = 2;
+double dogSpeed = 5;
+int friend = 2;
+double time;
+
+while(distance>10)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
